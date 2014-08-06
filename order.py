26a4673a8117f7ade9533266f39ed2d2e63f8ad5
Michael Klopot <michael.klopot@gmail.com>
2014-08-05 22:41:37 -0700

    print unit prices as part of the str()

--- order.py
+++ order.py
@@ -23,12 +23,13 @@ class Order(object):
         self.filled_time = None
         self.filled_price = None
         self.filled_by = None
+        self.unitprice = None
 
     def __str__(self):
         if self.ordertype == "SELL":
-            return "{1.num} {0:12} {1.ordertype:4} {1.sell_amount:12.6f} {1.sell_assetname:12} for              {1.buy_assetname:12}  user:{1.user.id:10}  status:{1.status:16}  price:{1.filled_price:}".format(type(self).__name__,self)
+            return "{1.num} {0:12} {1.ordertype:4} {1.sell_amount:12.6f} {1.sell_assetname:12} for              {1.buy_assetname:12}  user:{1.user.id:10}  status:{1.status:16}  price:{1.filled_price:}  unitprice:{1.unitprice}".format(type(self).__name__,self)
         else:
-            return "{1.num} {0:12} {1.ordertype:4} {1.buy_amount:12.6f} {1.buy_assetname:12} for              {1.sell_assetname:12}  user:{1.user.id:10}  status:{1.status:16}  price:{1.filled_price:}".format(type(self).__name__,self)
+            return "{1.num} {0:12} {1.ordertype:4} {1.buy_amount:12.6f} {1.buy_assetname:12} for              {1.sell_assetname:12}  user:{1.user.id:10}  status:{1.status:16}  price:{1.filled_price:}  unitprice:{1.unitprice}".format(type(self).__name__,self)
 
 class LimitOrder(Order):
     def __init__(self,user,limit,buy_amount=None,buy_assetname=None,sell_amount=None,sell_assetname=None):
@@ -37,9 +38,9 @@ class LimitOrder(Order):
 
     def __str__(self):
         if self.ordertype == "SELL":
-            return "{1.num} {0:12} {1.ordertype:4} {1.sell_amount:12.6f} {1.sell_assetname:12}  @  {1.limit:12.6f} {1.buy_assetname:12}  user:{1.user.id:10}  status:{1.status:16}  price:{1.filled_price:}".format(type(self).__name__,self)
+            return "{1.num} {0:12} {1.ordertype:4} {1.sell_amount:12.6f} {1.sell_assetname:12}  @  {1.limit:12.6f} {1.buy_assetname:12}  user:{1.user.id:10}  status:{1.status:16}  price:{1.filled_price:}  unitprice:{1.unitprice}".format(type(self).__name__,self)
         else: 
-            return "{1.num} {0:12} {1.ordertype:4} {1.buy_amount:12.6f} {1.buy_assetname:12}  @  {1.limit:12.6f} {1.sell_assetname:12}  user:{1.user.id:10}  status:{1.status:16}  price:{1.filled_price:}".format(type(self).__name__,self)
+            return "{1.num} {0:12} {1.ordertype:4} {1.buy_amount:12.6f} {1.buy_assetname:12}  @  {1.limit:12.6f} {1.sell_assetname:12}  user:{1.user.id:10}  status:{1.status:16}  price:{1.filled_price:}  unitprice:{1.unitprice}".format(type(self).__name__,self)
 
 class MarketOrder(Order):
     pass
